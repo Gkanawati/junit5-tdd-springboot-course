26ca2a8e1f2020f7580b4ce0f673eec14b7c5aab
gkanawati <gabrielkanawati3@gmail.com>
2026-01-21 08:12:45 -0300

    feat: create unit test for all methods of SimpleMathTest

--- 1-unit-tests-mvn/src/test/java/br/com/gkanawati/math/SimpleMathTest.java
+++ 1-unit-tests-mvn/src/test/java/br/com/gkanawati/math/SimpleMathTest.java
@@ -1,37 +1,91 @@
 package br.com.gkanawati.math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * This class contains unit tests for the {@link SimpleMath} class.
+ */
 class SimpleMathTest {
 
   @Test
-  void sum() {
+  void testSum() {
     SimpleMath simpleMath = new SimpleMath();
-    Double actual = simpleMath.sum(6.2D, 2D);
-    Double expected = 8.2D;
+    double firstNumber = 6.2D;
+    double secondNumber = 2D;
 
-    assertEquals(expected, actual);
+    Double actual = simpleMath.sum(firstNumber, secondNumber);
+    double expected = 8.2D;
+
+    assertNotNull(actual);
+    // lazily using a lambda expression (`() -> {}`) to build the fail message
+    // when assertion fails, the message is built and displayed
+    assertEquals(expected, actual, () -> firstNumber + " + " + secondNumber + " must be " + expected);
   }
 
   @Test
-  void subtraction() {
+  void testSubtraction() {
+    SimpleMath simpleMath = new SimpleMath();
+    double firstNumber = 6.2D;
+    double secondNumber = 2D;
+
+    Double actual = simpleMath.subtraction(firstNumber, secondNumber);
+    double expected = 4.2D;
+
+    assertNotNull(actual);
+    assertEquals(expected, actual, () -> firstNumber + " - " + secondNumber + " must be " + expected);
   }
 
   @Test
-  void multiplication() {
+  void testMultiplication() {
+    SimpleMath simpleMath = new SimpleMath();
+    double firstNumber = 6.2D;
+    double secondNumber = 2D;
+
+    Double actual = simpleMath.multiplication(firstNumber, secondNumber);
+    double expected = 12.4D;
+
+    assertNotNull(actual);
+    assertEquals(expected, actual, () -> firstNumber + " * " + secondNumber + " must be " + expected);
   }
 
   @Test
-  void division() {
+  void testDivision() {
+    SimpleMath simpleMath = new SimpleMath();
+    double firstNumber = 6.2D;
+    double secondNumber = 2D;
+
+    Double actual = simpleMath.division(firstNumber, secondNumber);
+    double expected = 3.1D;
+
+    assertNotNull(actual);
+    assertEquals(expected, actual, () -> firstNumber + " / " + secondNumber + " must be " + expected);
   }
 
   @Test
-  void mean() {
+  void testMean() {
+    SimpleMath simpleMath = new SimpleMath();
+    double firstNumber = 6.2D;
+    double secondNumber = 2D;
+
+    Double actual = simpleMath.mean(firstNumber, secondNumber);
+    double expected = 4.1D;
+
+    assertNotNull(actual);
+    assertEquals(expected, actual, () -> "Mean of " + firstNumber + " and " + secondNumber + " must be " + expected);
   }
 
   @Test
-  void squareRoot() {
+  void testSquareRoot() {
+    SimpleMath simpleMath = new SimpleMath();
+    double number = 81D;
+
+    Double actual = simpleMath.squareRoot(number);
+    double expected = 9D;
+
+    assertNotNull(actual);
+    assertEquals(expected, actual, () -> "Square root of " + number + " must be " + expected);
   }
 }

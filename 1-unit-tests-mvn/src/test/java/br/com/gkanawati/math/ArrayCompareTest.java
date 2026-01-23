0eed726e480c7649bc08b90f2f64e1da6ca7a21a
gkanawati <gabrielkanawati3@gmail.com>
2026-01-22 22:21:26 -0300

    feat: add performance test with timeout for sorting in ArrayCompareTest

--- 1-unit-tests-mvn/src/test/java/br/com/gkanawati/math/ArrayCompareTest.java
+++ 1-unit-tests-mvn/src/test/java/br/com/gkanawati/math/ArrayCompareTest.java
@@ -3,7 +3,9 @@ package br.com.gkanawati.math;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 /**
  * This class contains unit tests for the {@link SimpleMath} class.
@@ -18,6 +20,20 @@ class ArrayCompareTest {
     Arrays.sort(numbers);
 
     assertArrayEquals(numbers, expected);
+  }
+
+  // for unit tests, prefer timeouts of milliseconds
+  // for integration tests, prefer timeouts of seconds
+  @Test
+  @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+  void testSortPerformance() {
+    int[] numbers = {25, 8, 21, 32, 3};
+
+    for (int i = 0; i < 10000000; i++) {
+      numbers[0] = i;
+      Arrays.sort(numbers);
+    }
+
   }
 
 }

ea47aa62a4d330bf25e98f3796341fdc6d2e9133
gkanawati <gabrielkanawati3@gmail.com>
2026-01-26 21:11:04 -0300

    feat: add unit tests for mocking List behavior in CourseBusinessMockTest

--- 3-mockito-unit-tests/src/test/java/br/com/gkanawati/business/CourseBusinessMockTest.java
+++ 3-mockito-unit-tests/src/test/java/br/com/gkanawati/business/CourseBusinessMockTest.java
@@ -1,6 +1,7 @@
 package br.com.gkanawati.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -34,6 +35,47 @@ class CourseBusinessMockTest {
 
     // then
     assertEquals(4, courses.size());
+  }
+
+  @Test
+  void testMockingList_Size() {
+    // given
+    List<?> mockedList = mock(List.class);
+    when(mockedList.size()).thenReturn(999).thenReturn(100);
+
+    // when
+    int size1 = mockedList.size();
+    int size2 = mockedList.size(); // multiple calls
+
+    // then
+    assertEquals(999, size1);
+    assertEquals(100, size2);
+  }
+
+  @Test
+  void testMockingList_GetMustReturn_DifferentValues() {
+    // given
+    List<String> mockedList = mock(List.class);
+    when(mockedList.getFirst()).thenReturn("First Call").thenReturn("Second Call");
+
+    // when
+    String firstCall = mockedList.getFirst();
+    String secondCall = mockedList.getFirst(); // multiple calls
+
+    // then
+    assertEquals("First Call", firstCall);
+    assertEquals("Second Call", secondCall);
+  }
+
+  @Test
+  void testMockingList_GetIsCalledWith_ArgumentMatchers() {
+    // given
+    List<String> mockedList = mock(List.class);
+    when(mockedList.get(anyInt())).thenReturn("Element");
+
+    // when / then
+    assertEquals("Element", mockedList.get(anyInt()));
+    assertEquals("Element", mockedList.get(999));
   }
 
 }

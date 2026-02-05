5848b45580c4a79e110b6a9cb2b6ae278ab8c9fd
gkanawati <gabrielkanawati3@gmail.com>
2026-02-04 21:50:06 -0300

    feat: adapt PersonRepositoryTest to use integration test configuration - postgresql db

--- 4-testing-spring-boot-applications/spring-boot-testing/src/test/java/br/com/gkanawati/springboottesting/repositories/PersonRepositoryTest.java
+++ 4-testing-spring-boot-applications/spring-boot-testing/src/test/java/br/com/gkanawati/springboottesting/repositories/PersonRepositoryTest.java
@@ -5,18 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import br.com.gkanawati.springboottesting.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.gkanawati.springboottesting.model.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 /**
  * Unit tests for {@link PersonRepository}
  */
 @DataJpaTest
-class PersonRepositoryTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PersonRepositoryTest extends AbstractIntegrationTest {
 
   @Autowired
   PersonRepository personRepository;
